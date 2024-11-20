c27518d7698c817d70e303ed126c90ffd70f3547
Daiki TANAHARA <e245725@ie.u-ryukyu.ac.jp>
2024-11-20 19:27:03 +0900

    再提出用修正

--- app/src/main/java/jp/ac/uryukyu/ie/e245725/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245725/Enemy.java
@@ -68,12 +68,11 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        if (dead){
-            System.exit(0);
-        }
+        if (dead == false){
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
+        }
     }
 
     /**
